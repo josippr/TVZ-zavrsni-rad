05523200f4ae37dc8af6c09e7f71e2eb0310e4d4
Josip Prpić <47066785+josippr@users.noreply.github.com>
2023-08-15 00:27:08 +0200

    UI update - new color pallete for app UI
    UI color pallete update - from light to dark mood

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -1,16 +1,128 @@
 
 import './home.css';
 
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs, query, where } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { getAuth } from 'firebase/auth';
+
 function Explore() {
+
+    const [ticketCount, setTicketCount] = useState(0);
+    const[taskCount, setTaskCount] = useState(0);
+    const [postCount, setPostCount] = useState(0);
+
+
+    const auth = getAuth();
+
+    useEffect(() => {
+        const fetchTickets = async () => {
+        try {
+            const user = auth.currentUser;
+            if (!user) {
+            // User not logged in, handle this case if needed
+            return;
+            }
+
+            const userId = user.uid;
+            const listingsRef = collection(db, 'listings');
+            const q = query(listingsRef, where('userRef', '==', userId));
+            const querySnapshot = await getDocs(q);
+            const count = querySnapshot.size;
+            setTicketCount(count);
+        } catch (error) {
+            console.error('Error fetching tickets:', error);
+        }
+        };
+
+        fetchTickets();
+    }, [auth]);
+
+    useEffect(() => {
+        const fetchtasks = async () => {
+        try {
+            const user = auth.currentUser;
+            if (!user) {
+            // User not logged in, handle this case if needed
+            return;
+            }
+
+            const userId = user.uid;
+            const listingsRef = collection(db, 'workTasks');
+            const q = query(listingsRef, where('userRef', '==', userId));
+            const querySnapshot = await getDocs(q);
+            const count = querySnapshot.size;
+            setTaskCount(count);
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
+        }
+        };
+
+        fetchtasks();
+    }, [auth]);
+
+    /*
+    useEffect(() => {
+        const fetchposts = async () => {
+        try {
+            const user = auth.currentUser;
+            if (!user) {
+            // User not logged in, handle this case if needed
+            return;
+            }
+
+            const userId = user.uid;
+            const listingsRef = collection(db, 'posts');
+            const q = query(listingsRef, where('userRef', '==', userId));
+            const querySnapshot = await getDocs(q);
+            const count = querySnapshot.size;
+            setTaskCount(count);
+        } catch (error) {
+            console.error('Error fetching tasks:', error);
+        }
+        };
+
+        fetchposts();
+    }, [auth]);
+    */
+
+
+
     return (
         <div className="home">
             <h1>Home</h1>
             <div className='home-content'>
-                <div className='all-tickets-container'>
-                    
+                <div className='left'>
+                    <div className='tickets-number'>
+                        {ticketCount}
+                    </div>
+                    <div className='tickets-title'>
+                        Opened tickets
+                    </div>
                 </div>
-                <div className='create-tickets-container'>
-                    
+                <div className='right'>
+                    <div className='tickets-number'>
+                        {taskCount}
+                    </div>
+                    <div className='tickets-title'>
+                        My work tasks
+                    </div>
+                </div>
+                <div className='left-two'>
+                    <div className='tickets-number'>
+                        {postCount}
+                    </div>
+                    <div className='tickets-title'>
+                        My Social feed activity
+                    </div>
+                </div>
+                <div className='right'>
+                    <div className='tickets-number'>
+                        
+                    </div>
+                    <div className='tickets-title'>
+                        Total
+                    </div>
                 </div>
             </div>
         </div>
